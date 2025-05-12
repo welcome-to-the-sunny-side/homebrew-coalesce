cfb75ef3b9b73f0707613fcbda1d70b949bbc0bc
unalive <nonadhocproblems@gmail.com>
2025-05-12 15:32:56 +0530

    Update urllib3 to                                                                    correct URL and SHA

--- Formula/coalesce.rb
+++ Formula/coalesce.rb
@@ -8,7 +8,6 @@ class Coalesce < Formula
   license "MIT"
 
   depends_on "python@3.9"
-  depends_on "setuptools" => :build
 
   resource "certifi" do
     url "https://files.pythonhosted.org/packages/71/da/e94e26401b62acd6d91df2b52954aceb7f561743aa5ccc32152886c76c96/certifi-2024.2.2.tar.gz"
