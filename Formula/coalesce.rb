daf9c6a0ad25648dc7b403808470787d363935cf
unalive <nonadhocproblems@gmail.com>
2025-05-12 20:18:18 +0530

    changes

--- Formula/coalesce.rb
+++ Formula/coalesce.rb
@@ -4,7 +4,7 @@ class Coalesce < Formula
     desc "manage your solves"
     homepage "https://github.com/welcome-to-the-sunny-side/coalesce"
     url      "https://github.com/welcome-to-the-sunny-side/coalesce/archive/refs/tags/v0.1.2.tar.gz"
-    sha256   "aa7d052361d9be3eebe1a676b1806bf80e7bbfc9d83184d76d39db7f508d1bff"
+    sha256   "f0555e0663955b90304782d2b72bd855d36238030bf2eb4706cff33aee940dbe"
     license  "MIT"
     version  "0.1.2"
     head     "https://github.com/welcome-to-the-sunny-side/coalesce.git", branch: "main"
