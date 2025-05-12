1eb5748dcb24513ff357197860d6e3051fdcaef9
unalive <nonadhocproblems@gmail.com>
2025-05-12 14:42:48 +0530

    changes

--- Formula/coalesce.rb
+++ Formula/coalesce.rb
@@ -18,6 +18,11 @@ class Coalesce < Formula
       url "https://files.pythonhosted.org/packages/9d/ee/391076f5937f0a8cdf5e53b701ffc91753e87b07d66bae4a09aa671897bf/requests-2.28.2.tar.gz"
       sha256 "98b1b2782e3c6c4904938b84c0eb932721069dfdb9134313beff7c83c2df24bf"
     end
+
+    resource "urllib3" do
+        url "https://files.pythonhosted.org/packages/c5/52/fe421fb7364aa738b3506a2d99e4f3a56e079c0a798e9f4fa5e14c60922f/urllib3-1.26.15.tar.gz"
+        sha256 "8a388717b9476f934a21484e8c8e61875ab60644d29b9b39e11e4b9dc1c6b305"
+    end
   
     resource "tabulate" do
       url "https://files.pythonhosted.org/packages/7a/53/afac341569b3fd558bf2b5428e925e2eb8753ad9627c1f9188104c6e0c4a/tabulate-0.8.10.tar.gz"
