08a7f7f629caa2ade28249405725bd6092063f76
unalive <nonadhocproblems@gmail.com>
2025-05-12 15:21:04 +0530

    Update urllib3 to  correct URL and SHA

--- Formula/coalesce.rb
+++ Formula/coalesce.rb
@@ -41,7 +41,7 @@ class Coalesce < Formula
   
   resource "urllib3" do
     url "https://files.pythonhosted.org/packages/8a/78/16493d9c386d8e60e442a35feac5e00f0913c0f4b7c217c11e8ec2ff53e0/urllib3-2.4.0.tar.gz"
-    sha256 "51d94ec3955ef3dcc21ca51067e18407bcc91b618159c58dcbc81a860bbca080"
+    sha256 "414bc6535b787febd7567804cc015fee39daab8ad86268f1310a9250697de466"
   end
   
     def install
